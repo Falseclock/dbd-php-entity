95a1d2616c91019464097f1678090a0c131498aa
Nurlan Mukhanov <102@mp.kz>
2019-09-10 20:49:26 +0600

    XML metadata

--- src/Primitive.php
+++ src/Primitive.php
@@ -8,7 +8,6 @@ use MyCLabs\Enum\Enum;
  * Class Primitive
  *
  * @see     https://docs.oasis-open.org/odata/odata-csdl-xml/v4.01/csprd05/odata-csdl-xml-v4.01-csprd05.html#sec_PrimitiveTypes
-
  * @method static Primitive Binary()
  * @method static Primitive Boolean()
  * @method static Primitive Byte()
@@ -29,6 +28,7 @@ use MyCLabs\Enum\Enum;
  */
 class Primitive extends Enum
 {
+	private const BOOLEAN = "boolean";
 	/** @var string Binary data */
 	public const Binary = "Binary";
 	/** @var string Binary-valued logic */
@@ -44,9 +44,11 @@ class Primitive extends Enum
 	/** @var string IEEE 754 binary64 floating-point number (15-17 decimal digits) */
 	public const Double = "Double";
 	/** @var string Signed duration in days, hours, minutes, and (sub)seconds */
-	public const Duration = "Duration";
+	public const  Duration = "Duration";
+	private const FLOAT    = "float";
 	/** @var string 16-byte (128-bit) unique identifier */
-	public const Guid = "Guid";
+	public const  Guid    = "Guid";
+	private const INTEGER = "int";
 	/** @var string Signed 16-bit integer */
 	public const Int16 = "Int16";
 	/** @var string Signed 32-bit integer */
@@ -54,7 +56,8 @@ class Primitive extends Enum
 	/** @var string Signed 64-bit integer */
 	public const Int64 = "Int64";
 	/** @var string Signed 8-bit integer */
-	public const SByte = "SByte";
+	public const  SByte  = "SByte";
+	private const STRING = "string";
 	/** @var string IEEE 754 binary32 floating-point number (6-9 decimal digits) */
 	public const Single = "Single";
 	/** @var string Binary data stream */
@@ -63,4 +66,30 @@ class Primitive extends Enum
 	public const String = "String";
 	/** @var string Clock time 00:00-23:59:59.999999999999 */
 	public const TimeOfDay = "TimeOfDay";
+
+	/**
+	 * If you need to set your own types - just extend this class and override this method
+	 *
+	 * @return string
+	 */
+	public function getPhpVarType() {
+		switch($this->value) {
+			case self::Int16:
+			case self::Int32:
+			case self::Int64:
+				return self::INTEGER;
+				break;
+
+			case self::Boolean;
+				return self::BOOLEAN;
+				break;
+
+			case self::Double:
+			case self::Single;
+				return self::FLOAT;
+				break;
+			default:
+				return self::STRING;
+		}
+	}
 }
