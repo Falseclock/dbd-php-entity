343dcd5a5c1ad5bcb913fff8a9a136025fed481f
Nurlan Mukhanov <102@mp.kz>
2020-04-08 19:37:02 +0600

    NULLS LAST

--- src/Order.php
+++ src/Order.php
@@ -6,8 +6,10 @@ use Exception;
 
 final class Order
 {
-	const ASCENDING  = "ASC";
-	const DESCENDING = "DESC";
+	const ASCENDING             = "ASC";
+	const ASCENDING_NULLS_LAST  = "ASC NULLS LAST";
+	const DESCENDING            = "DESC";
+	const DESCENDING_NULLS_LAST = "DESC NULLS LAST";
 
 	/**
 	 * Order constructor.
