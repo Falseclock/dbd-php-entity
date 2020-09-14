984c2332caba5652e1080b0290bd8be7cb6ccb3b
Nurlan Mukhanov <102@mp.kz>
2020-09-14 12:21:22 +0600

    synthetic

---
+++ src/Interfaces/SyntheticEntity.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Synthetic Entity
+ *
+ * @author       Written by Nurlan Mukhanov <nurike@gmail.com>, March 2020
+ */
+
+namespace DBD\Entity\Interfaces;
+
+interface SyntheticEntity
+{
+}
