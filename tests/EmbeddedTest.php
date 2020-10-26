08d275e8ac3486746b084fcd29f935caf0aff0dc
Nurlan Mukhanov <102@mp.kz>
2020-10-26 12:08:15 +0600

    key tests

--- tests/EmbeddedTest.php
+++ tests/EmbeddedTest.php
@@ -18,6 +18,8 @@
  *                                                                              *
  ********************************************************************************/
 
+declare(strict_types=1);
+
 namespace DBD\Entity\Tests;
 
 use DBD\Entity\Common\MapperException;
